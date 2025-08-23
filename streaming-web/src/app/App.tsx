ca5b42a18bba4adb44af8f0cd8944cfbda10687b
molotskyb <molotskyb@gmail.com>
2025-08-23 14:48:10 +0300

    styling fixes

--- streaming-web/src/app/App.tsx
+++ streaming-web/src/app/App.tsx
@@ -5,8 +5,32 @@ import { ENV } from "../config/env";
 export default function App() {
 	return (
 		<PlayerProvider>
-			<div style={{ padding: 16, width: 1260, margin: "0 auto" }}>
-				<h1>Dash Player Seed</h1>
+			<div
+				style={{
+					padding: 16,
+					width: "100vw",
+					maxWidth: 1260,
+					margin: "0 auto",
+					boxSizing: "border-box",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "flex-start",
+					minHeight: "100vh",
+				}}
+			>
+				<img
+					src="/assets/logo.png"
+					alt="Logo"
+					style={{
+						display: "block",
+						margin: "32px auto 16px auto",
+						maxWidth: "180px",
+						width: "40vw",
+						height: "auto",
+					}}
+				/>
+				<h1 style={{ textAlign: "center" }}>Dash Player Seed</h1>
 				<VideoPlayer
 					mpdUrl={ENV.MPD}
 					drm={{
@@ -15,7 +39,11 @@ export default function App() {
 						headers: ENV.DRM_HEADERS,
 					}}
 				/>
-				<p style={{ opacity: 0.7 }}>Source: {ENV.MPD}</p>
+				<p
+					style={{ opacity: 0.7, wordBreak: "break-all", textAlign: "center" }}
+				>
+					Source: {ENV.MPD}
+				</p>
 			</div>
 		</PlayerProvider>
 	);
