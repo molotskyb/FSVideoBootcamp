69695bcccf63169a1ba4939335e89935d6dce33b
molotskyb <molotskyb@gmail.com>
2025-08-30 16:38:26 +0300

    background color changes and cenentering the content vertically and horizontally.

--- streaming-web/src/app/Layout.tsx
+++ streaming-web/src/app/Layout.tsx
@@ -4,10 +4,23 @@ import Nav from "./Nav";
 
 export default function Layout() {
 	return (
-		<div style={{ padding: 16, maxWidth: 1260, margin: "0 auto" }}>
-			<h1>Streaming Demos</h1>
-			<Nav /> {/* ✅ inside router context */}
-			<Outlet /> {/* where child routes render */}
-		</div>
+		<>
+			<img
+				src="../assets/logo.png"
+				alt="logo"
+				style={{
+					position: "fixed",
+					top: 16,
+					left: 16,
+					height: 164,
+					zIndex: 1000,
+				}}
+			/>
+			<div style={{ padding: 16, maxWidth: 1260, margin: "0 auto" }}>
+				<h1>Streaming Demos</h1>
+				<Nav />
+				<Outlet />
+			</div>
+		</>
 	);
 }
